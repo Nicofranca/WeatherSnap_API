ebcf0a16704da8cc9c7e3508224e50e040a39f23
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-05 21:15:16 -0300

    feat: finaliza botao de escolha JS

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const cidade = "Jaraguá do Sul"
-const url2 = `https://geocoding-api.open-meteo.com/v1/search?name=${cidade}&count=1&language=pt&format=json`
-fetch(url2)
+const botao = document.getElementById('btn-busca');
+const form = document.getElementById('form-busca');
+const input = document.getElementById('input-cidade');
+const textoCidade = document.getElementById('cidade-texto')
+
+botao.addEventListener('click', () => {
+    
+    botao.classList.add('hidden');
+
+    form.classList.remove('hidden');
+
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); //nao deixa a pagina recarre no ente
+
+    const cidade = input.value;
+
+    textoCidade.innerText = cidade;
+    form.classList.add('hidden');
+    botao.classList.remove('hidden');
+
+
+    
+console.log("Cidade: "+cidade)
+
+const urlGeoApi = `https://geocoding-api.open-meteo.com/v1/search?name=${cidade}&count=1&language=pt&format=json`
+fetch(urlGeoApi)
 
     .then((response) => response.json())
 
@@ -12,10 +37,10 @@ fetch(url2)
         console.log(latitude)
         console.log(longitude)
 
-        const url = `https://api.open-meteo.com/v1/forecast?&latitude=${latitude}&longitude=${longitude}&current_weather=true&daily=temperature_2m_max,temperature_2m_min,weathercode&timezone=auto`;
+        const urlClimaApi = `https://api.open-meteo.com/v1/forecast?&latitude=${latitude}&longitude=${longitude}&current_weather=true&daily=temperature_2m_max,temperature_2m_min,weathercode&timezone=auto`;
 
         
-        fetch(url)
+        fetch(urlClimaApi)
             .then((response) => response.json())
             .then((data) => { 
     
@@ -27,15 +52,4 @@ fetch(url2)
 
 })
 
-
-const botao = document.getElementById('btn-busca');
-const form = document.getElementById('form-busca');
-const input = document.getElementById('input-cidade');
-
-botao.addEventListener('click', () => {
-    
-    botao.classList.add('hidden');
-
-    form.classList.remove('hidden');
-
-});
+})
